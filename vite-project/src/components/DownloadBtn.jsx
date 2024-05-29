b5650380309ad8064425ed9d8cffcbf918606d1d
haluktutuncu <112336855+haluktutuncu@users.noreply.github.com>
2024-05-29 20:53:16 +0300

    v.1.1

--- vite-project/src/components/DownloadBtn.jsx
+++ vite-project/src/components/DownloadBtn.jsx
@@ -2,7 +2,7 @@ import utils from "../utils/utils";
 
 export default function DownloadBtn() {
   return (
-    <div className="border-white bg-black text-white border-b-2 relative transition-all duration-500 ease-in-out">
+    <div className="border-white bg-black text-white relative transition-all duration-500 ease-in-out">
       <button
         onClick={() => utils("../public/downloads/Haluk.pdf", "haluk.pdf")}
         className="w-full py-[3%] pl-[2%] md:py-[1.5%] md:pl-[2%] lg:py-[1.3%] lg:pl-[1%] text-left flex justify-between focus:outline-none focus-visible:ring focus-visible:ring-purple-500 items-center"
